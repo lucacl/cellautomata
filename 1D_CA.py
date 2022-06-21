f52ed1f6cf1b9462f161bc2d7492bad5b7fc7bb5
lucacl <107915687+lucacl@users.noreply.github.com>
2022-06-21 12:17:30 +0200

    Update 1D_CA.py

--- 1D_CA.py
+++ 1D_CA.py
@@ -1,3 +1,16 @@
 from CA import CA
 
-class 1D_CA(CA):
+class CA_1D(CA):
+    
+    def init_neighbours(self):
+        for i, cell in enumerate(self.grid):
+            for j in self.neighbourhood:
+                k = i + j
+                if k < 0 or k >= len(self.grid):
+                    if self.edge_type == "wrap":
+                        x = self.grid[k % len(self.grid)]
+                    elif self.edge_type == "mirror":
+                        x = cell
+                    elif self.edge_type.isdigit():
+                        x = int(self.edge_type)
+                cell.neighbourhood.append(x)
